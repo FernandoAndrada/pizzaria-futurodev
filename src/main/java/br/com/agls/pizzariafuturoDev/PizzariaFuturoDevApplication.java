971707e31ab1723c95466b1e235add1c12256ea7
FernandoAndrada <fernandojcandrada@gmail.com>
2022-10-14 21:09:50 -0300

    Cria pizzaria

---
+++ src/main/java/br/com/agls/pizzariafuturoDev/PizzariaFuturoDevApplication.java
@@ -0,0 +1,13 @@
+package br.com.agls.pizzariafuturoDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaFuturoDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaFuturoDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/agls/pizzariafuturoDev/PizzariaFuturoDevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agls.pizzariafuturoDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaFuturoDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
